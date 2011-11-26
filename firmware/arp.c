83918f9aab2f357be2e0dd897ce78b5505a6f9b3
Ptolom <ptolom@hexifact.co.uk>
2011-11-26 22:29:27 +0000

    working on arp table hash

--- firmware/arp.c
+++ firmware/arp.c
@@ -3,9 +3,26 @@
 
 typedef struct {
   mac_address mac;
-  IP_address;
+  IP_address ip;
 } arp_entry;
 arp_entry arp_hash[16];
+arp_entry arp_hash_bucket[4];
 uint8_t reverse_arp_hash[16];
+arp_add(IP_address ip, mac_address mac)
+{
+  uint8_t index = h(ip);
+  arp_hash[index].mac=mac;
+  arp_hash[index].ip=ip;
+  reverse_arp_hash[h(mac)] = index;
+}
 
-arp_lookup(IP_address;
+arp_lookup(IP_address ip);
+{
+  uint8_t index= h(ip);
+  if (arp_hash[index].ip=0)
+    return 0;
+  else if (arp_hash[index].ip=ip)
+    return arp_hash[index].mac;
+  else
+
+
