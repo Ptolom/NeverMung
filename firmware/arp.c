848bada1c4da98651f8fbd539fbdcb2fa6c4a1c4
Ptolom <ptolom@hexifact.co.uk>
2011-11-29 22:41:38 +0000

    hash function

--- firmware/arp.c
+++ firmware/arp.c
@@ -1,5 +1,6 @@
 #include <flags.h>
 #include <types.h>
+#define ARP_HASH_SIZE 32
 
 typedef struct {
   mac_address mac;
@@ -8,12 +9,31 @@ typedef struct {
 arp_entry arp_hash[16];
 arp_entry arp_hash_bucket[4];
 uint8_t reverse_arp_hash[16];
+hash_ip(IP_address ip)
+{
+  uint8_t keysize=log(2,ARP_HASH_SIZE); //move to init function and make static
+  uint8_t shunt_1, shunt_2
+  shunt_1=  (16-keysize)/2+keysize;
+  shunt_2=  (16-keysize)/2;
+  if (keysize%2 ==1)
+    shunt_2++;
+
+  uint8_t temp =ip.c[6]*ip.c[6];
+  temp = temp << shunt_1
+
 arp_add(IP_address ip, mac_address mac)
 {
-  uint8_t index = h(ip);
+  uint8_t home, index, i;
+  home = index = hash_ip(ip);
+  for (i=0;(arp_hash[index].mac!=0)&&i<ARP_HASH_SIZE;i++){
+    index=home + (i*i+i)/2;
+  }
+  if (i==ARP_HASH_SIZE){
+    set(ARP_HASH_FULL);
+  }
   arp_hash[index].mac=mac;
   arp_hash[index].ip=ip;
-  reverse_arp_hash[h(mac)] = index;
+  reverse_arp_hash[hash_mac(mac)] = index;
 }
 
 arp_lookup(IP_address ip);
